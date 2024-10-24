ee62906d5a630a8b6c51e75cc6ac2b3d6a3dfd1b
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-10-23 20:50:07 -0600

    feat(validation): Implement validation for integers

--- internal/validation/integers.go
+++ internal/validation/integers.go
@@ -1,12 +1,18 @@
 package validation
 
-func checkInt(value any) int {
-	result, ok := value.(int)
-	if !ok {
+func checkInt(value any) int64 {
+	switch value.(type) {
+	case int8:
+		return int64(value.(int8))
+	case int16:
+		return int64(value.(int16))
+	case int32:
+		return int64(value.(int32))
+	case int64:
+		return value.(int64)
+	default:
 		panic("validating wrong types")
 	}
-
-	return result
 }
 
 func IntCheckNatural(message string) ValidationFunc {
