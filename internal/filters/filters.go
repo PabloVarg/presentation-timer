064318f0995517a86ec2ada2f7358966b09348a9
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2024-10-25 21:21:09 -0600

    feat(filters): Implement filters package for pagination

---
+++ internal/filters/filters.go
@@ -0,0 +1,83 @@
+package filters
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/PabloVarg/presentation-timer/internal/helpers"
+	"github.com/PabloVarg/presentation-timer/internal/validation"
+)
+
+type Filters struct {
+	Page           int32
+	PageSize       int32
+	SortBy         string
+	SafeSortFields []string
+}
+
+func FromRequest(r *http.Request, defaultPageSize int32, safeSortFields ...string) (Filters, validation.Validator) {
+	v := validation.New()
+
+	page, err := helpers.QueryInt32(r, "page", 1)
+	if err != nil {
+		v.AddErrors("page", "not a valid number")
+	}
+
+	pageSize, err := helpers.QueryInt32(r, "page_size", defaultPageSize)
+	if err != nil {
+		v.AddErrors("page_size", "not a valid number")
+	}
+
+	result := Filters{
+		Page:           page,
+		PageSize:       pageSize,
+		SortBy:         r.URL.Query().Get("sort_by"),
+		SafeSortFields: safeSortFields,
+	}
+	result.Validate(v)
+
+	return result, v
+}
+
+func (f Filters) Validate(v validation.Validator) {
+	v.Check(
+		"page",
+		f.Page,
+		validation.IntCheckNatural("can not be negative"),
+		validation.IntCheckMax(10_000_000, "exceeded maximum page limit"),
+	)
+	v.Check(
+		"page_size",
+		f.PageSize,
+		validation.IntCheckNatural("can not be negative"),
+		validation.IntCheckMax(100, "maximum page size is 100"),
+	)
+	if f.SortBy == "" {
+		return
+	}
+	v.Check(
+		"sort_by",
+		strings.TrimPrefix(f.SortBy, "-"),
+		validation.StringCheckIn(f.SafeSortFields, "invalid value"),
+	)
+}
+
+func (f Filters) QueryLimit() int32 {
+	return f.PageSize
+}
+
+func (f Filters) QueryOffset() int32 {
+	return (f.Page - 1) * f.PageSize
+}
+
+func (f Filters) QuerySortDirection() string {
+	if strings.HasPrefix(f.SortBy, "-") {
+		return "DESC"
+	}
+
+	return "ASC"
+}
+
+func (f Filters) QuerySortBy() string {
+	return strings.TrimPrefix(f.SortBy, "-")
+}
