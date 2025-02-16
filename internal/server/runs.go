bacef5928d419f80d81a46c28db04ccf06344271
Pablo Vargas <49590991+PabloVarg@users.noreply.github.com>
2025-02-16 14:53:25 -0600

    feat(runs): Finish implementation of runs

--- internal/server/runs.go
+++ internal/server/runs.go
@@ -1,14 +1,13 @@
 package server
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
 	"errors"
 	"fmt"
 	"log/slog"
 	"net/http"
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/PabloVarg/presentation-timer/internal/helpers"
@@ -18,7 +17,20 @@ import (
 	"golang.org/x/sync/errgroup"
 )
 
+type RunStatusResponse struct {
+	State  string          `json:"state"`
+	Step   queries.Section `json:"step"`
+	MsLeft int64           `json:"ms_left"`
+	// errors
+	Err error `json:"error,omitempty"`
+}
+
 func RunPresentation(logger *slog.Logger, queriesStore *queries.Queries) http.Handler {
+	type input struct {
+		Action string `json:"action"`
+		Step   *int32 `json:"step"`
+	}
+
 	upgrader := websocket.Upgrader{
 		ReadBufferSize:  1024,
 		WriteBufferSize: 1024,
@@ -39,7 +51,11 @@ func RunPresentation(logger *slog.Logger, queriesStore *queries.Queries) http.Ha
 		}
 
 		if _, ok := runs[ID]; !ok {
-			runs[ID] = NewRun(ID, logger, queriesStore)
+			runs[ID], err = NewRun(ID, logger, queriesStore)
+			if err != nil {
+				helpers.InternalError(w, logger, err)
+				return
+			}
 		}
 
 		conn, err := upgrader.Upgrade(w, r, nil)
@@ -64,25 +80,32 @@ func RunPresentation(logger *slog.Logger, queriesStore *queries.Queries) http.Ha
 				return
 			}
 
-			fields := strings.Fields(string(p))
-			switch {
-			case fields[0] == "start":
+			var message input
+			if err := json.NewDecoder(bytes.NewReader(p)).Decode(&message); err != nil {
+				conn.WriteJSON(helpers.ErrorResponse{
+					Error: err.Error(),
+				})
+				continue
+			}
+
+			switch message.Action {
+			case "status":
+				runs[ID].SendMsg(Status, WithConn(conn))
+			case "start":
 				runs[ID].SendMsg(StartPresentation, WithConn(conn))
-			case fields[0] == "pause":
+			case "pause":
 				runs[ID].SendMsg(PausePresentation, WithConn(conn))
-			case fields[0] == "resume":
+			case "resume":
 				runs[ID].SendMsg(ResumePresentation, WithConn(conn))
-			case fields[0] == "step":
-				if len(fields) != 2 {
-					continue
+			case "step":
+				if message.Step == nil {
+					conn.WriteJSON(helpers.ErrorResponse{
+						Error: "a step must be given for the step action",
+					})
+					break
 				}
 
-				step, err := strconv.Atoi(fields[1])
-				if err != nil {
-					continue
-				}
-
-				runs[ID].SendMsg(StepInto, WithStep(int32(step)), WithConn(conn))
+				runs[ID].SendMsg(StepInto, WithStep(*message.Step), WithConn(conn))
 			default:
 				conn.WriteJSON(map[string]any{
 					"error": "command not recognized",
@@ -97,17 +120,17 @@ type RunTask struct {
 	presentationID int64
 	logger         *slog.Logger
 	conns          map[string]*websocket.Conn
-	queriesStore   *queries.Queries
 	// sections
 	sections []queries.Section
 	// runs state
-	ctx            context.Context
-	cancel         context.CancelFunc
-	timer          *time.Timer
-	timerEnd       time.Time // Stores end of timer, for pause events
-	timerRemaining time.Duration
-	step           int32
-	msg            chan TaskMsg
+	isRunning     bool
+	ctx           context.Context
+	cancel        context.CancelFunc
+	timer         *time.Timer
+	timerEnd      time.Time     // Stores end of timer, for pause events
+	timeRemaining time.Duration // Stores the time remaining for next step on pause events
+	step          int32
+	msg           chan TaskMsg
 }
 
 type TaskMsg struct {
@@ -117,27 +140,34 @@ type TaskMsg struct {
 }
 
 const (
-	StartPresentation  = iota
-	PausePresentation  = iota
-	ResumePresentation = iota
+	StartPresentation = iota
+	PausePresentation
+	ResumePresentation
 	StepInto
+	Status
 )
 
 func NewRun(
 	presentationID int64,
 	logger *slog.Logger,
 	queriesStore *queries.Queries,
-) RunTask {
+) (RunTask, error) {
 	stoppedTimer := time.NewTimer(0)
 	stoppedTimer.Stop()
 
-	ctx, cancel := context.WithCancel(context.Background())
+	dbCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	sections, err := queriesStore.GetSectionsByPosition(dbCtx, presentationID)
+	if err != nil {
+		return RunTask{}, err
+	}
 
+	ctx, cancel := context.WithCancel(context.Background())
 	task := RunTask{
 		presentationID: presentationID,
 		logger:         logger,
-		queriesStore:   queriesStore,
 		conns:          make(map[string]*websocket.Conn),
+		sections:       sections,
 		// runs state
 		ctx:    ctx,
 		cancel: cancel,
@@ -146,7 +176,7 @@ func NewRun(
 	}
 	go task.Run()
 
-	return task
+	return task, nil
 }
 
 func (t RunTask) AddConnection(ID string, conn *websocket.Conn) {
@@ -180,22 +210,20 @@ func (t *RunTask) Run() {
 		case <-t.timer.C:
 			t.logger.Info("pr run", "tick", "tock")
 			t.step += 1
+			t.timerEnd = time.Time{}
+
 			if int(t.step) >= len(t.sections) {
+				t.step -= 1
 				t.timer.Stop()
-				t.Broadcast(map[string]any{
-					"state": "end",
-				})
+				t.isRunning = false
+				t.Broadcast(t.GetRunState())
 				continue
 			}
 
 			t.timer = time.NewTimer(t.sections[t.step].Duration)
 			t.timerEnd = time.Now().Add(t.sections[t.step].Duration)
 
-			t.Broadcast(map[string]any{
-				"step":         t.sections[t.step].Name,
-				"finish_time":  t.timerEnd,
-				"remaining_ms": t.sections[t.step].Duration.Milliseconds(),
-			})
+			t.Broadcast(t.GetRunState())
 		case msg := <-t.msg:
 			if err := t.HandleMsg(msg); err != nil {
 				t.RespondToMsg(msg, map[string]any{
@@ -210,39 +238,33 @@ func (t *RunTask) HandleMsg(msg TaskMsg) error {
 	t.logger.Info("handle message", "msg", msg, "action", msg.action)
 
 	switch msg.action {
+	case Status:
+		t.Broadcast(t.GetRunState())
 	case StartPresentation:
 		t.logger.Info("handle message", "case", "start presentation")
-		dbCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-
-		sections, err := t.queriesStore.GetSectionsByPosition(dbCtx, t.presentationID)
-		if err != nil {
-			return err
-		}
-
-		t.sections = sections
 		t.step = -1
 		t.timer = time.NewTimer(0)
+		t.isRunning = true
 	case PausePresentation:
 		t.logger.Info("handle message", "case", "pause presentation")
-		t.timerRemaining = t.timerEnd.Sub(time.Now())
-		t.timer.Stop()
+		if t.timeRemaining == 0 {
+			t.timeRemaining = t.timerEnd.Sub(time.Now())
+			t.timerEnd = time.Time{}
+			t.timer.Stop()
+			t.isRunning = false
+		}
 
-		t.Broadcast(map[string]any{
-			"state":        "paused",
-			"step":         t.sections[t.step].Name,
-			"remaining_ms": t.timerRemaining.Milliseconds(),
-		})
+		t.Broadcast(t.GetRunState())
 	case ResumePresentation:
 		t.logger.Info("handle message", "case", "resume presentation")
-		t.timer = time.NewTimer(t.timerRemaining)
-		t.timerEnd = time.Now().Add(t.timerRemaining)
+		if t.timeRemaining != 0 {
+			t.timer = time.NewTimer(t.timeRemaining)
+			t.timerEnd = time.Now().Add(t.timeRemaining)
+			t.timeRemaining = 0
+			t.isRunning = true
+		}
 
-		t.Broadcast(map[string]any{
-			"state":        "running",
-			"step":         t.sections[t.step].Name,
-			"remaining_ms": t.timerRemaining.Milliseconds(),
-		})
+		t.Broadcast(t.GetRunState())
 	case StepInto:
 		t.logger.Info("handle message", "case", "step presentation")
 		if msg.targetStep < 0 || int(msg.targetStep) >= len(t.sections) {
@@ -254,6 +276,33 @@ func (t *RunTask) HandleMsg(msg TaskMsg) error {
 	}
 
 	return nil
+}
+
+func (t RunTask) GetRunState() RunStatusResponse {
+	step := t.step
+	if step < 0 {
+		step = 0
+	}
+	if step >= int32(len(t.sections)) {
+		step = int32(len(t.sections)) - 1
+	}
+
+	state := RunStatusResponse{
+		State:  "running",
+		Step:   t.sections[t.step],
+		MsLeft: t.timerEnd.Sub(time.Now()).Milliseconds(),
+	}
+
+	if !t.isRunning {
+		state.State = "stopped"
+		state.MsLeft = t.timeRemaining.Milliseconds()
+	}
+
+	if t.timeRemaining == 0 && t.timerEnd.IsZero() {
+		state.MsLeft = 0
+	}
+
+	return state
 }
 
 func (t RunTask) SendMsg(action int, opts ...func(*TaskMsg)) {
